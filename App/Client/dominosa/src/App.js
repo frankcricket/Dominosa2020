24703b50e605ec6266fb4ba265e28d04e7296d9d
BlackMamba <scarfonemarco@gmail.com>
2020-12-13 12:13:14 +0100

    Final Commit

---
+++ App/Client/dominosa/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Game from './components/Game'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Game></Game>
+  );
+}
+
+export default App;
